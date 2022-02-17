ba95e7ff95ef8c79db34314d72964ac194fad64c
Senol E <whereimdb@gmail.com>
2022-02-17 14:45:14 -0600

    Dockerfile and Jenkinsfile updated

--- alarm-backend/run.py
+++ alarm-backend/run.py
@@ -1,6 +1,6 @@
 from sqlalchemy import true
 from alarm import app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="4444", debug=True)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port="4444", debug=True)
 
